2c41f881e90b52eeaad5ffe7a17fdf23566a3710
rikerik <bako.erik.dominik@gmail.com>
2023-03-29 21:10:22 +0200

    input, lines, bet

--- project.js
+++ project.js
@@ -6,7 +6,7 @@
 // 6. Give money to user
 // 7. Play again
 
-const prompt = require("prompt-sync")();
+const prompt = require("prompt-sync")(); // for the input
 
 const deposit = () => {
     while (true) {
@@ -25,18 +25,32 @@ const deposit = () => {
 const getNumberOfLines = () => {
     while (true) {
         const lines = prompt("Enter the number of lines to bet on (1-3): ");
-        const NumberOfLines = parseFloat(lines);
+        const numberOfLines = parseFloat(lines);
 
-        if (isNaN(NumberOfLines) || NumberOfLines <= 0 ||  NumberOfLines > 3) {
+        if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
             console.log("Invalid number of lines");
         }
         else {
-            return NumberOfLines;
+            return numberOfLines;
         }
     }
 };
 
+const getBet = (balance, lines) => {
+    while (true) {
+        const bet = prompt("Enter the bet per line: ");
+        const numberBet = parseFloat(bet);
 
-let Balance = deposit();
-const NumberOfLines = getNumberOfLines();
+        if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines) {
+            console.log("Invalid bet");
+        }
+        else {
+            return numberBet;
+        }
+    }
+};
+
+let balance = deposit();
+const numberOfLines = getNumberOfLines();
+const bet = getBet(balance, numberOfLines);
 
